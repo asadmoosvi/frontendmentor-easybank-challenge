79bbf887802b3d9f54be6df93771c8059634445f
Asad Moosvi <moosvi.asad@gmail.com>
2021-07-23 18:56:18 +0500

    Add hamburger animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
