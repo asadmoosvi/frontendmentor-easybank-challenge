1d9e7f204bce4c62f8572717aa9b8acf03f1da12
Asad Moosvi <moosvi.asad@gmail.com>
2021-07-24 00:51:41 +0500

    Finish mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -3,4 +3,5 @@ const header = document.querySelector('.header');
 
 btnHamburger.addEventListener("click", () => {
   header.classList.toggle("open");
+  document.body.classList.toggle('noscroll');
 });
