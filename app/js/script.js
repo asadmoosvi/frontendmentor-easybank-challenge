6d45fcdb6b14727949975c2ac9b67ba20010b270
Asad Moosvi <moosvi.asad@gmail.com>
2021-07-25 00:15:50 +0500

    Use javascript for fading in and out elements

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,27 @@
 const btnHamburger = document.querySelector("#btnHamburger");
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   header.classList.toggle("open");
-  document.body.classList.toggle('noscroll');
+  document.body.classList.toggle("noscroll");
+  if (header.classList.contains("open")) {
+    fadeInElems(fadeElems);
+  } else {
+    fadeOutElems(fadeElems);
+  }
 });
+
+function fadeInElems(elems) {
+  elems.forEach((elem) => {
+    elem.classList.remove("fade-out");
+    elem.classList.add("fade-in");
+  });
+}
+
+function fadeOutElems(elems) {
+  elems.forEach((elem) => {
+    elem.classList.remove("fade-in");
+    elem.classList.add("fade-out");
+  });
+}
